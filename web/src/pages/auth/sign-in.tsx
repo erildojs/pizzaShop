e7509f8254c5bf628ba6270c6980103b96f744ec
ErildoJS <erildofranciscojs@gmail.com>
2025-12-15 07:18:56 +0100

    feat: configure seo with react helmet asybc

--- web/src/pages/auth/sign-in.tsx
+++ web/src/pages/auth/sign-in.tsx
@@ -1,7 +1,10 @@
+import { Helmet } from "react-helmet-async"
+
 export function SignIn() {
   return (
-    <div>
+    <>
+    <Helmet title="SignIn"/>
       <h1>Dashboard</h1>
-    </div>
+    </>
   )
 }
