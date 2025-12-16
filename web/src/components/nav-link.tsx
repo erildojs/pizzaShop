90ff09e4ed7c6293a648a50f74c19274fdc481a5
ErildoJS <erildofranciscojs@gmail.com>
2025-12-16 07:05:08 +0100

    feat: change color of link when is active

--- web/src/components/nav-link.tsx
+++ web/src/components/nav-link.tsx
@@ -1,9 +1,18 @@
-import { Link, type LinkProps } from "react-router";
+import { Link, useLocation, type LinkProps } from "react-router";
 
 export type NavLinkProps = LinkProps
 
 export function NavLink(props: NavLinkProps) {
+  const {pathname} = useLocation() //useLocation retorna infos da rota
   return (
-    <Link className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground" {...props}/>
+    <Link 
+    data-current={pathname === props.to}
+    className="flex items-center gap-1.5 text-sm font-medium text-muted-foreground hover:text-foreground
+      data-[current=true]:text-foreground
+    " {...props}
+    
+    />
   )
 }
+
+//props.to - endereço para onde o link vai
