c8ff5d5c24fe710df31463284b5affc5d2df4825
ErildoJS <erildofranciscojs@gmail.com>
2025-12-22 08:29:04 +0100

    feat: show name of restaurant in header

--- web/src/components/account-menu.tsx
+++ web/src/components/account-menu.tsx
@@ -1,27 +1,40 @@
 import { Building, ChevronDown, LogOut } from "lucide-react";
 import { Button } from "./ui/button";
 import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { useQuery } from "@tanstack/react-query";
+import { getProfile } from "@/api/get-profile";
+import { getManagedRestaurant } from "@/api/get-managed-restaurant";
 
 export function AccontMenu() {
+  const { data: profile } = useQuery({
+    queryKey: ['profile'],
+    queryFn: getProfile
+  })//useQuery para operaçoes de busca
+
+  const { data: managedRestaurant } = useQuery({
+    queryKey: ['managed-restaurant'],
+    queryFn: getManagedRestaurant
+  })//useQuery para operaçoes de busca
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" className="flex items-center gap-2 select-none">Pizza Shop
-          <ChevronDown className="w-4 h-4"/>
+        <Button variant="outline" className="flex items-center gap-2 select-none">{managedRestaurant?.name}
+          <ChevronDown className="w-4 h-4" />
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="w-56">
         <DropdownMenuLabel className="flex flex-col">
-          <span>Erildo</span>
-          <span className="tex-xs font-normal text-muted-foreground">erildo@gmail.com</span>
+          <span>{profile?.name}</span>
+          <span className="tex-xs font-normal text-muted-foreground">{profile?.email}</span>
         </DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuItem>
-          <Building className="w-4 h-4 mr-2"/>
+          <Building className="w-4 h-4 mr-2" />
           <span>Perfil da loja</span>
         </DropdownMenuItem>
         <DropdownMenuItem className="text-rose-500 dark:text-rose-400">
-          <LogOut className="w-4 h-4 mr-2 dark:text-rose-400"/>
+          <LogOut className="w-4 h-4 mr-2 dark:text-rose-400" />
           <span>Sair</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -3,4 +3,5 @@ import axios from "axios"
 
 export const api = axios.create({
   baseURL: env.VITE_API_URL,
+  withCredentials: true//faz com que os cookies do front-end sao enviados para o back-end
 })
