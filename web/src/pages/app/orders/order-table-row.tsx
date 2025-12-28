b259ee80769df5eeb2fc645f943625ae9c610e96
ErildoJS <erildofranciscojs@gmail.com>
2025-12-28 08:06:44 +0100

    feat: list all oders

--- web/src/pages/app/orders/order-table-row.tsx
+++ web/src/pages/app/orders/order-table-row.tsx
@@ -3,51 +3,64 @@ import { Dialog, DialogTrigger } from "@/components/ui/dialog";
 import { TableCell, TableRow } from "@/components/ui/table";
 import { ArrowRight, Search } from "lucide-react";
 import { OrderDetails } from "./order-details";
+import { OrderStatus } from "@/components/order-status";
+import { ptBR } from "date-fns/locale"
+import { formatDistanceToNow } from "date-fns"
 
-export function OrderTableRow() {
+export interface OrderTableRowProps {
+  order: {
+    orderId: string;
+    createdAt: string;
+    status: "pending" | "processing" | "delivering" | "delivered" | "canceled";
+    customerName: string;
+    total: number;
+  }
+}
+export function OrderTableRow({ order }: OrderTableRowProps) {
   return (
     <TableRow>
       <TableCell>
         <Dialog>
           <DialogTrigger asChild>
-              <Button variant="outline" size="sm">
-                <Search className="w-3 h-3"/>
-                <span className="sr-only">Detalhes do pedido</span>
-              </Button>
+            <Button variant="outline" size="sm">
+              <Search className="w-3 h-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
           </DialogTrigger>
           <OrderDetails />
         </Dialog>
-            </TableCell>
-            <TableCell className="font-mono text-xs font-medium">
-              5382958735728954829647
-            </TableCell>
-            <TableCell className="text-muted-foreground">
-              há 15 minutos
-            </TableCell>
-            <TableCell>
-              <div className="flex items-center gap-2">
-                <span className="h-2 w-2 rounded-full bg-slate-400" />
-                <span className="font-medium text-muted-foreground">
-                  Pendente
-                </span>
-              </div>
-            </TableCell>
-            <TableCell className="font-medium">Erildo</TableCell>
-            <TableCell className="font-medium">
-              Kz 15.000,00
-            </TableCell>
-            <TableCell>
-              <Button variant="outline" size="sm">
-                <ArrowRight className="mr-2 h-3 w-3" />
-                Aprovar
-              </Button>
-            </TableCell>
-            <TableCell>
-              <Button variant="ghost" size="sm">
-                <ArrowRight className="mr-2 h-3 w-3" />
-                Aprovar
-              </Button>
-            </TableCell>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        5382958735728954829647
+      </TableCell>
+      <TableCell className="text-muted-foreground">
+        {formatDistanceToNow(order.createdAt, {
+          locale: ptBR,
+          addSuffix: true//coloca o há tanto tempo
+        })}
+      </TableCell>
+      <TableCell>
+        <OrderStatus status={order.status} />
+      </TableCell>
+      <TableCell className="font-medium">{order.customerName}</TableCell>
+      <TableCell className="font-medium">
+        {order.total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL'
+        })}
+      </TableCell>
+      <TableCell>
+        <Button variant="outline" size="sm">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="sm">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
     </TableRow>
   )
 }
--- web/src/pages/app/orders/orders.tsx
+++ web/src/pages/app/orders/orders.tsx
@@ -3,42 +3,50 @@ import { Helmet } from "react-helmet-async";
 import { OrderTableRow } from "./order-table-row";
 import { OrderTableFilters } from "./order-table-filters";
 import { Pagination } from "@/components/pagination";
+import { useQuery } from "@tanstack/react-query";
+import { getOrders } from "@/api/get-orders";
 
 export function Orders() {
+  const { data: result } = useQuery({
+    queryKey: ['orders'],
+    queryFn: getOrders
+  })
   return (
     <>
-    <Helmet title="Pedidos"/>
-    <div className="flex flex-col gap-4">
-      <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
-    <div className="space-y-2.5">
-      <OrderTableFilters />
+      <Helmet title="Pedidos" />
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
+        <div className="space-y-2.5">
+          <OrderTableFilters />
 
-      <div className="border rounded-md">
-        <Table>
-          <TableHeader>
-            <TableRow>
-            <TableHead className="w-[64px]"></TableHead>
-            <TableHead className="w-[140px]">Id</TableHead>
-            <TableHead className="w-[180px]">realizado há</TableHead>
-            <TableHead className="w-[140px]">Status</TableHead>
-            <TableHead>Cliente</TableHead>
-            <TableHead className="w-[140px]">Total do Pedido</TableHead>
-            <TableHead className="w-[164px]"></TableHead>
-            <TableHead className="w-[132px]"></TableHead>
+          <div className="border rounded-md">
+            <Table>
+              <TableHeader>
+                <TableRow>
+                  <TableHead className="w-[64px]"></TableHead>
+                  <TableHead className="w-[140px]">Id</TableHead>
+                  <TableHead className="w-[180px]">realizado há</TableHead>
+                  <TableHead className="w-[140px]">Status</TableHead>
+                  <TableHead>Cliente</TableHead>
+                  <TableHead className="w-[140px]">Total do Pedido</TableHead>
+                  <TableHead className="w-[164px]"></TableHead>
+                  <TableHead className="w-[132px]"></TableHead>
 
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <OrderTableRow />
-          </TableBody>
-        </Table>
+                </TableRow>
+              </TableHeader>
+              <TableBody>
+                {result && result.orders.map(order => {
+                  return <OrderTableRow key={order.orderId} order={order} />
+                })}
+              </TableBody>
+            </Table>
+          </div>
+
+          <Pagination pageIndex={0} totalCount={105} perPage={10} />
         </div>
-
-        <Pagination pageIndex={0} totalCount={105} perPage={10}/>
       </div>
-    </div>
     </>
-    
-    
+
+
   )
 }
